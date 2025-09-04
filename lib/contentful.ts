a2f227d0bbdd42dad747cbcf6ff966286ebd6843
Akshaykeerti <akshaykeerti@gmail.com>
2025-09-04 17:11:02 +0400

    Add comment to Contentful client configuration

--- lib/contentful.ts
+++ lib/contentful.ts
@@ -1,5 +1,6 @@
 import { createClient } from 'contentful'
 
+// Contentful CMS client configuration
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID || '',
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || '',
